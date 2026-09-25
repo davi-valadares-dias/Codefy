8c262eaafe0f5212e73ba64e44efbbaee9c80274
Davi Valadares Dias <davivaladarescontato@gmail.com>
2026-09-25 00:27:23 -0300

    feat: configura mapeamento de categorias e tecnologias

---
+++ backend/Codefy.Infrastructure/Persistence/Configurations/TechnologyConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codefy.Infrastructure.Persistence.Configurations
+{
+    internal class TechnologyConfiguration
+    {
+    }
+}
